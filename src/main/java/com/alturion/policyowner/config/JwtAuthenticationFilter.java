e5ebeb46a0240c7190a6e75a14714e4732a702fa
Anbarasan <anbarasan.rkav@gmail.com>
2026-03-19 13:13:30 +0530

    Implemented SecurityConfig and added jwtAuthenticationFilter

--- src/main/java/com/alturion/policyowner/config/JwtAuthenticationFilter.java
+++ src/main/java/com/alturion/policyowner/config/JwtAuthenticationFilter.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 
@@ -17,6 +18,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
 	
 	private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class); 
